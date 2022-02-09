d5590a5ac542d502dd76714a5372482c2f5f03c3
Sheba Jose <shjose@bestpeers.com>
2022-02-09 15:50:55 +0530

    Added More

--- src/index.js
+++ src/index.js
@@ -1,39 +1,129 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css';
-import { BrowserRouter as Router, Routes, Route, Navigate, Link } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+  Link,
+  Outlet,
+  useParams,
+  NavLink,
+  useNavigate,
+  useLocation,
+} from "react-router-dom";
 
 ReactDOM.render(
   <Router>
-  <Routes>
-    <Route path="/" element={<Home/>} />
-    <Route path="/myapps" element={<Navigate replace to="/learn"/>} />
-    <Route path="/learn" element={<Learn/>} />
-  </Routes>
-</Router>,
-  document.getElementById('root')
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/myapps" element={<Navigate replace to="/learn" />} />
+      <Route path="learn" element={<Learn />}>
+        <Route path="courses" element={<Courses />}>
+          <Route path=":courseid" element={<CourseId />} />
+        </Route>
+        <Route path="bundles" element={<Bundles />} />
+      </Route>
+      <Route path="dashboard" element={<DashBoard />} />
+    </Routes>
+  </Router>,
+  document.getElementById("root")
 );
 
-function Home(){
-  return(
-  <div>
-    <h1> Home Route </h1>
+function Home() {
+  return (
+    <div>
+      <h1> Home Route </h1>
     </div>
   );
 }
 
-function Learn(){
-  return(
-  <div>
-    <h1> Learn </h1>
-    <h4>All the course are here </h4>
-    <Link className="btn btn-primary" to="/learn/course">Courses</Link> |
-    <Link className="btn btn-success" to="/learn/bundle">Bundle</Link>
+function Learn() {
+  return (
+    <div>
+      <h1>Learn</h1>
+      <h4>All courses are listed here</h4>
+      <Link className="btn btn-success" to="/learn/courses">
+        courses
+      </Link>{" "}
+      |
+      <Link className="btn btn-primary" to="/learn/bundles">
+        bundle
+      </Link>
+      <Outlet />
     </div>
   );
 }
 
+function Courses() {
+  const courseList = ["React", "Angular", "Vue", "Nodejs"];
+  const randomCourseName =
+    courseList[Math.floor(Math.random() * courseList.length)];
+  return (
+    <div>
+      <h1> Courses List </h1>
+      <h4> Courses Card </h4>
+      <p>More Test</p>
+      <NavLink
+        style={({ isActive }) => {
+          return {
+            backgroundColor: isActive ? "pink" : "yellow",
+          };
+        }}
+        to={`/learn/courses/${randomCourseName}`}
+      >
+        {randomCourseName}
+      </NavLink>
+      |
+      <NavLink className="btn btn-outline-primary" to={`/learn/courses/tests`}>
+        tests
+      </NavLink>
+      <Outlet />
+    </div>
+  );
+}
+
+function Bundles() {
+  return (
+    <div>
+      <h1> Bundle List </h1>
+      <h4> Bundle Card </h4>
+    </div>
+  );
+}
+
+function CourseId() {
+  const navigate = useNavigate();
+  const { courseid } = useParams();
+  return (
+    <div>
+      <h1> URL Params is :- {courseid} </h1>
+      <button
+        className="btn btn-warning"
+        onClick={() => {
+          // navigate("/dashboard", { state: "399" });
+          navigate("/dashboard", { state: courseid });
+        }}
+      >
+        Price
+      </button>
+      <Link to="/dashboard" state={"DJANGO"}>
+        Test link
+      </Link>
+    </div>
+  );
+}
+
+function DashBoard() {
+  const location = useLocation();
+  return (
+    <div>
+      <h1> Info that i got here is :- {location.state} </h1>
+    </div>
+  );
+}
 
 reportWebVitals();
